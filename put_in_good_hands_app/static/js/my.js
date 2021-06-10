184c023803b5dc4e8272b87efca11f70ad75dd81
andree0 <jonek.andree@gmail.com>
2021-06-11 00:40:22 +0200

    convert js to jquery

--- put_in_good_hands_app/static/js/my.js
+++ put_in_good_hands_app/static/js/my.js
@@ -1,94 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form#multistep");
-    const btnNext = form.querySelectorAll(".next-step");
-    const btnPrev = form.querySelectorAll(".prev-step");
-    const divForm = form.querySelectorAll("div[data-step]");
-    const divCounterStep = document.querySelector("div.form--steps-counter");
-    const spanCounter = divCounterStep.querySelector("span");
-    const pStep = document.querySelectorAll("div.form--steps-container p");
-    const waringDiv = document.querySelector("div.form--steps-instructions");
-
+$(document).ready(function() {
     let currentStep = 1
 
     function warring(pStep) {
-        pStep.forEach(p => {
-            if (p.dataset.step == currentStep) {
-                p.classList.add("active");
+        pStep.each(function () {
+            if ($(this).data("step") == currentStep) {
+                $(this).addClass("active")
             } else {
-                p.className = "";
+                $(this).removeClass("active")
             }
         })
     }
 
     function activeDiv(divForm) {
-        divForm.forEach(div => {
-            if (div.dataset.step == currentStep) {
-                div.classList.add("active")
+        divForm.each(function () {
+            if ($(this).data("step") == currentStep) {
+                $(this).addClass("active")
             } else {
-                div.className = ""
+                $(this).removeClass("active")
             }
         })
     }
 
-    btnNext.forEach(btn => {
-        btn.addEventListener("click", () => {
-            if (currentStep == 1) {
-                filterCategory();
-            }
-            currentStep++
-            spanCounter.innerText = currentStep
-            activeDiv(divForm);
-            warring(pStep);
-            if (currentStep == 5) {
-                waringDiv.setAttribute("hidden", "");
-            }
-        })
-    })
-
-
-    btnPrev.forEach(btn => {
-        btn.addEventListener("click", () => {
-            currentStep--
-            spanCounter.innerText = currentStep
-            activeDiv(divForm);
-            warring(pStep);
-            if (currentStep != 5) {
-                waringDiv.removeAttribute("hidden");
-            }
-        })
-    })
-
-    const checkboxSpanCategories = document.querySelectorAll(
-        "span.checkbox");
-
     function filterCategory() {
-        const categories = JSON.parse(document.getElementById(
-            "categories").textContent)
-        const institutions = JSON.parse(document.getElementById(
-            "institutions").textContent)
+        const institutions = JSON.parse($("#institutions").text())
         institutions[0] = '{';
-        categories[0] = '{';
         institutions[-1] = '}';
-        categories[-1] = '}';
-        const institutionObjects = JSON.parse(institutions);
-        const categoryObjects = JSON.parse(categories);
-        institutionObjects.forEach((i) => {
-            const inputInst = document.querySelector(
-                `div[data-step="3"] input[value="${i.pk}"`);
-            const divStep3 = inputInst.parentElement.parentElement
-            divStep3.setAttribute("hidden", "")
-            const iCat = i.fields.categories
+        $(JSON.parse(institutions)).each(function () {
+            const divStep3 = $(`div[data-step="3"] input[value="${$(this)
+                    .prop("pk")}"`).parent().parent()
+            divStep3.hide(this)
+            const iCat = $($(this).prop("fields"))
+                .attr("categories")
             if (iCat.length) {
-                console.log(inputInst);
-                console.log(iCat);
-                checkboxSpanCategories.forEach(cat => {
-                    const inputCat = cat.previousElementSibling
-                    if (inputCat.checked) {
-                        console.log(inputCat);
-                        iCat.forEach((c) => {
-                            console.log("dziła");
-                            if (c == inputCat.value) {
-                                divStep3.removeAttribute("hidden")
+                $("div[data-step='1'] span.checkbox").each(function () {
+                    const inputCat = $(this).prev()
+                    if ($(inputCat).prop( "checked" )) {
+                        iCat.forEach(function (c) {
+                            if (c == $(inputCat).val()) {
+                                divStep3.show(this)
                             }
                         })
                     }
@@ -97,9 +46,26 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
-    function confirmData() {
-        const divSummary = document.querySelector("div.summary");
-        const bagInput = document.querySelector("input[name='bags']");
+    $("form#multistep .next-step").click(function () {
+        if (currentStep == 1) {
+            filterCategory();
+        }
+        currentStep++
+        $("div.form--steps-counter span").text(currentStep)
+        activeDiv($("div[data-step]"));
+        warring($("div.form--steps-container p"));
+        if (currentStep == 5) {
+            $("div.form--steps-instructions").hide();
+        }
+    })
 
-    }
+    $("form#multistep .prev-step").click(function () {
+        currentStep--
+        $("div.form--steps-counter span").text(currentStep)
+        activeDiv($("div[data-step]"));
+        warring($("div.form--steps-container p"));
+        if (currentStep != 5) {
+            $("div.form--steps-instructions").show();
+        }
+    })
 })
